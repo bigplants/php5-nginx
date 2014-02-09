63ec7b953efc2397987d00b509b82bf2487415c6
bigplants <hirakatad@isao.co.jp>
2014-02-09 23:16:56 +0900

    change php5-imagick version

--- omusubi/attributes/default.rb
+++ omusubi/attributes/default.rb
@@ -15,5 +15,5 @@ default['versions']['mysql-server'] = '5.5.*'
 default['versions']['postgresql'] = '9.1*'
 default['versions']['curl'] = '7.22.0*'
 default['versions']['imagemagick'] = '8:6.6.9.7*'
-default['versions']['php5-imagick'] = '3.1.0*'
+default['versions']['php5-imagick'] = '3.*'
 
